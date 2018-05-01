2a45de83db211a20b5ba1443710f6ba2a6faf2b5
Jeswin <jeswincyriac.k@gmail.com>
2018-05-01 15:32:56 +0530

    finalisinglogin

--- android/app/components/Register.js
+++ android/app/components/Register.js
@@ -3,21 +3,24 @@ import {
   StyleSheet,
   Text,
   View,
-  TextInput,
-  KeyboardAvoidingView,
   TouchableOpacity,
   Picker,
-
+  ScrollView,
+  KeyboardAvoidingView
 } from 'react-native';
-
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import DateTimePicker from 'react-native-modal-datetime-picker';
+import TextInput from 'react-native-material-textinput';
+ import Icon from 'react-native-vector-icons/FontAwesome';
 var {bp, vw, vh} = require('react-native-relative-units')(375);
 import DatePicker from 'react-native-datepicker';
 let valuelog = true;
 export default class Register extends React.Component {
     constructor(props){
       super(props)
-      this.state = {date:"",
-                    batch:"",}//
+      this.state = {dob1:new Date(0),
+                    batch:"",
+                    isDateTimePickerVisible: false,}//
     }
 
     login = ()=>{
@@ -29,212 +32,201 @@ export default class Register extends React.Component {
         this.props.nest("Profile");
     }
 
+  _showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
+
+  _hideDateTimePicker = () => this.setState({ isDateTimePickerVisible: false });
+
+  _handleDobPicked = (date) => {
+     this.setState({dob1:date})
+
+    this._hideDateTimePicker();
+  };
+
   render() {
     return(
 
-    <View  style = {styles.wrapper}>
-            <View  style={styles.form}>
 
-                 <TextInput
-                  style={styles.textInput} placeholder='Name(As in college records)'
-                  placeholderTextColor='rgba(255,255,255,0.7)'
-                  //onChangeText={(password) => this.setState({password})}
-                  //fontStyle={this.state.password.length == 0 ? 'italic' : 'normal'}
-                  underlineColorAndroid='black'
-                 />
-                 <TextInput
-                  style={styles.textInput} placeholder='Email '
-                  placeholderTextColor='rgba(255,255,255,0.7)'
-                  //onChangeText={(password) => this.setState({password})}
-                  //fontStyle={this.state.password.length == 0 ? 'italic' : 'normal'}
-                  underlineColorAndroid='black'
-                 />
-                 <TextInput
-                  style={styles.textInput} placeholder='rollnumber'
-                  placeholderTextColor='rgba(255,255,255,0.7)'
-                  //onChangeText={(password) => this.setState({password})}
-                  //fontStyle={this.state.password.length == 0 ? 'italic' : 'normal'}
-                  underlineColorAndroid='black'
-                 />
+         <View style = {{
+            // backgroundColor:"rgba(255,255,255,0.5)",
+             justifyContent:"center",
+             alignItems:"center",
+             flex:1,
 
+         }}  behavior="height">
+         <View style={{
+            // backgroundColor:"rgba(255,255,255,0.5)"
 
-                <DatePicker
-                 style={{width: 200}}
-                 mode="date"
-                 placeholder="Date Of Birth"
-                 date={this.state.date}
-                 format="YYYY-MM-DD"
-                 minDate="2016-05-01"
-                 maxDate="2016-06-01"
-                 confirmBtnText="Confirm"
-                 cancelBtnText="Cancel"
-                 customStyles={{
-                   dateIcon: {
-                     position: 'absolute',
-                     left: 0,
-                     top: 4,
-                     marginLeft: 0
-                   },
-                   dateInput: {
-                     marginLeft: 36,
-                   },
-                   dateText: {
-                    color: '#fff'
-                    },
-                   // ... You can check the source to find the other keys.
-                 }}
-                 onDateChange={(date) => {this.setState({date: date})}}
-                />
-
-                <DatePicker
-                 style={{width: 200}}
-                 mode="date"
-                 placeholder="passout year couldnt find an year picker may use number picker"
-                 date={this.state.date}
-                 format="YYYY-MM-DD"
-                 minDate="2016-05-01"
-                 maxDate="2016-06-01"
-                 confirmBtnText="Confirm"
-                 cancelBtnText="Cancel"
-                 customStyles={{
-                   dateIcon: {
-                     position: 'absolute',
-                     left: 0,
-                     top: 4,
-                     marginLeft: 0,
-                    backgroundColor:'rgba(0,0,0,0.7)',
-                   },
-                   dateInput: {
-                     marginLeft: 36,
-                     backgroundColor:'rgba(0,0,0,0.7)',
-                   },
-                   dateText: {
-                    color: '#fff'
-                    },
-                   // ... You can check the source to find the other keys.
-                 }}
-                 onDateChange={(date) => {this.setState({date: date})}}
-                />
-                <View style={{flexDirection: 'row',
-                          height:50,
-
-                      alignItems:'center'
-                       }}>
-
-
-                   <Text style={{color:"#fff"}}>Branch  </Text>
-
-                    <Picker
-                          selectedValue={this.state.batch}
-                          style={{ height: 20, width: 150 ,
-                                    color:"#000",
-                                    backgroundColor:"#fff",
-                                    }}
-                          onValueChange={(itemValue, itemIndex) => this.setState({batch: itemValue})}
-                          mode='dropdown'>
-                          <Picker.Item label="-select-" value="NULL" />
-                          <Picker.Item label="CS" value="CS" />
-                          <Picker.Item label="EC" value="EC" />
-                          <Picker.Item label="EEE" value="EEE" />
-                          <Picker.Item label="EB" value="EB" />
-
-                     </Picker>
-
-
-
-                </View>
-
-
-            </View>
-
-            <TouchableOpacity  style={styles.register}
-                      onPress={this.register}  >
-                    <Text style={{
-                        fontWeight:'bold',
-                        fontSize:21,
-                        color:"#fff"
-                              }}>Register</Text>
-            </TouchableOpacity>
+         }}>
             <View style={{
-                         //height:40,
-                         width:100 * vw,
-                         justifyContent:"flex-end",
-                         flex:1,
-                         backgroundColor:'rgba(255,255,255,0)',
-                                              }}>
+                width:vw*80
+            }}>
+                 <TextInput
+                  style={{
+                        width:vw*80
+                  }}
+                  label="Name as in college records"
+                  labelColor="rgba(255,255,255,0.8)"
+                  labelActiveColor="#90CAF9"
+                  color="#fff"
+                  underlineColor="#fff"
+                  underlineActiveColor="#90CAF9"
+                  autoCorrect={false}
+                 />
+            </View>
+            <View style={{
+                width:vw*80
+            }}>
 
-                    <TouchableOpacity style={{backgroundColor:"rgba(0,0,0,.5)",
-                                  height:40,
-                                  width:100*vw,
-                                  justifyContent:"center",
-                                  alignItems:"center",
-                                 /// borderRadius:10,
-                                  //marginBottom:2
-                              }}
-                         onPress= {this.login} >
+            <TextInput
+             style={{
 
-                        <Text style={{
-                                    color:"#fff",
-                                    fontSize:20
-                            }}>Already Registered? Login</Text>
+             }}
+             label="Email"
+             autoCorrect={false}
+             labelColor="rgba(255,255,255,0.8)"
+             labelActiveColor="#90CAF9"
+             color="#fff"
+             underlineColor="#fff"
+             underlineActiveColor="#90CAF9"
 
-                    </TouchableOpacity>
+            />
 
             </View>
-    </View>
+            <View style={{
+                width:vw*80
+            }}>
+
+            <TextInput
+             style={{
+
+             }}
+             label="College Rollnumber"
+             autoCorrect={false}
+             labelColor="rgba(255,255,255,0.8)"
+             labelActiveColor="#90CAF9"
+             color="#fff"
+             underlineColor="#fff"
+             underlineActiveColor="#90CAF9"
+
+            />
+
+            </View>
+
+
+            <View style={{
+                flexDirection:"row"
+            }}>
+                    <TouchableOpacity onPress={this._showDateTimePicker}
+                    style={{
+                        flexDirection:"row",
+                        //marginTop:10,
+                        justifyContent:"center",
+                        alignItems:"flex-end",
+                    }}>
+
+                    <Text style={{
+                        color:"rgba(255,255,255,0.8)",
+                    }}>Date of birth:</Text>
+
+                    <DateTimePicker
+                    isVisible={this.state.isDateTimePickerVisible}
+                      onConfirm={this._handleDobPicked}
+                      onCancel={this._hideDateTimePicker}/>
+                      <Text style={{
+                       textDecorationLine: 'underline',
+                       color:"#ffff",
+
+                   }}>  {this.state.dob1.getDate()}-{this.state.dob1.getMonth()+1}-{this.state.dob1.getYear()+1900} </Text>
+
+                    <Icon name="calendar" color="#fff" size={24} style={{marginLeft:10}}/>
+                      </TouchableOpacity>
+            </View>
+            <View style={{flexDirection: 'row',
+                      height:50,
+
+                  alignItems:'center'
+                   }}>
+
+
+               <Text style={{color:"rgba(255,255,255,0.8)"}}>Branch  </Text>
+
+                <Picker
+                      selectedValue={this.state.batch}
+                      style={{ height: 20, width: 150 ,
+                                color:"#000",
+                                backgroundColor:"rgba(255,255,255,0.8)"
+                                }}
+                      onValueChange={(itemValue, itemIndex) => this.setState({batch: itemValue})}
+                      mode='dropdown'>
+                      <Picker.Item label="-select-" value="NULL" />
+                      <Picker.Item label="CS" value="CS" />
+                      <Picker.Item label="EC" value="EC" />
+                      <Picker.Item label="EEE" value="EEE" />
+                      <Picker.Item label="EB" value="EB" />
+
+                 </Picker>
+            </View>
+            <View style={{
+                alignItems:"center",
+                justifyContent:"center",
+                marginTop:20
+            }}>
+                    <TouchableOpacity  style={{
+                        height:48,
+                        //backgroundColor:"rgba(255,255,255,0.8)",
+                        alignItems:"center",
+                        justifyContent:"center",
+                        padding:16
+
+                    }}
+                              onPress={this.register}  >
+                                    <View  style={{
+                                        height:36,
+                                        alignItems:"center",
+                                        justifyContent:"center",
+                                        borderRadius:100,
+                                        backgroundColor:"rgba(168,168,168,.3)",
+                                        padding:16,
+                                        borderColor:"#fff",
+                                        borderWidth:2
+
+                                    }}>
+                                    <Text style={{
+                                        fontFamily:"sanserif-medium",
+                                        fontSize:21,
+                                        color:"#fff"
+                                              }}>Register</Text>
+                                    </View>
+                    </TouchableOpacity>
+            </View>
+
+          </View>
+
+
+          <TouchableOpacity style={{
+               backgroundColor:'rgba(0,0,0,0.3)',
+               flexDirection:"row",
+               justifyContent:"center",
+               alignItems:"center",
+               height:48,
+               width:vw*100,
+               //alignSelf:"stretch",
+               position:"absolute",
+               bottom:0
+
+
+          }} onPress= {this.login}>
+
+          <Text style={{
+                                    color:"#fff",
+                                    fontSize:18,
+                                    fontFamily:"sanserif-medium"
+                            }}>Already Registered?   LOG IN</Text>
+
+          </TouchableOpacity>
+         </View>
 
 
     );
-  }
-
-
-
-
+    }
 }
-const styles =StyleSheet.create({
-
-  wrapper:{
-      flex: 1,
-      alignItems:'center',
-      justifyContent:'center',
-     //backgroundColor:'#607D8B',
-
-
- },
- form:{
-     //height:100,
-     width:250,
-    //alignItems:"space-around",
-     justifyContent:"space-around",
-
-     marginTop:vh * 25,
-     backgroundColor:'rgba(0,0,0,0.7)',
-
- },
- textInput:{
-   alignSelf:'stretch',
- //  padding:5,
-
-   color: '#fff',
-   height:50,
-   fontSize:18,
-   marginTop:10,
-   backgroundColor:'rgba(0,0,0,0.7)',
-
- },
- register:{
-     backgroundColor:"#0099FF",
-     //marginTop:100,
-     height:60,
-     flexDirection:"row",
-     // background color must be setssss
-     //borderWidth: 1,
-     //borderColor: 'rgba(168,168,168,.2)',
-     borderRadius:100,
-     width:180,
-     justifyContent:"center",
-    alignItems:"center",
-    marginTop:20,
- }
-
-});
